01e51070f0645e900b65b03fdb23e31ed7f4d89f
Teerth-the-coder <154464588+Teerth-the-coder@users.noreply.github.com>
2024-06-17 18:42:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function identify() {
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/lHNZCCyhK/model.json", modelLoaded);
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/W6WGKHTo5/model.json", modelLoaded);
 }
 
 function modelLoaded() {
@@ -14,15 +14,38 @@ function gotResults(error, result) {
         console.error(error);
     } else {
         console.log(result);
+        document.getElementById("resultLabel").innerHTML = "I Can Hear - " + result[0].label;
+        document.getElementById("resultAccuracy").innerHTML = "Accuracy - " + result[0].confidence.toFixed(2);
+
+        if (result[0].label == 'Pig') {
+            document.getElementById("image").src = "Pig.jpg";
+        } else if (result[0].label == 'Monkey') {
+            document.getElementById("image").src = "Monkey.jpg";
+        } else if (result[0].label == 'Cow') {
+            document.getElementById("image").src = "Cow.jpg";
+        } else {
+            document.getElementById("image").src = "Speaker.jpg";
+        }
     }
 }
 
 
 
-// {label: 'Backpack', confidence: 0.9945693612098694}
-
-// {label: 'Background Noise', confidence: 0.003012805711477995}
-
-// {label: 'Fork And Bowl', confidence: 0.0020427112467586994}
-
-// {label: 'Medicine',}
+/*[
+    {
+        "label": "Pig",
+        "confidence": 0.4571627974510193
+    },
+    {
+        "label": "Background Noise",
+        "confidence": 0.20363536477088928
+    },
+    {
+        "label": "Monkey",
+        "confidence": 0.177247554063797
+    },
+    {
+        "label": "Cow",
+        "confidence": 0.16195432841777802
+    }
+]*/
